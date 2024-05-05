11cbecfeddfc16d76f3b39660103e66d9f1bb3a1
Laércio Santos <laersantos13@hotmail.com>
2024-05-05 19:28:44 +0100

    val e var

---
+++ logica-programacao-kotlin-2/src/val e var.kt
@@ -0,0 +1,64 @@
+/*
+
+   Bem vindo a segunda etapa.
+
+   Para essa fase tens de:
+        - Ler e interpretar (Importantíssimo);
+        - criar variáveis (val ou var) de modo a resolveres o problema.
+
+        O que ganhas:
+         - Melhoramento de lógica programação;
+         - Noção do que fazer para tomada de decisões;
+         - Aperfeiçoar o uso do que aprendeste no "prólogo";
+         - Uso de val e var com impressão do conteúdo.
+
+         Texto:
+
+         Variáveis, como em toda outra linguagem de programação, são usadas para guardar dados e manipular-los
+         para satisfazer as necessidades do desenvolvedor para conclusão de um programa.
+
+         Tipos de dados: servem para indicar o exato valor que queiramos guardar numa variável.
+
+         Pensavas que seria algo longo? Isso não é história (u_u)
+
+
+         Exercício 1:
+
+               - Declarar uma variável com o tipo de dado que achar necessário (forma explicita) e guardar
+               o seu primeiro e último nome. A variável deve conter um nome auto-explicativo do que ela guarda;
+
+               - Declarar uma variável com o tipo de dado que achar necessário (forma explicita) e guardar
+               o número 1232, imprimir essa variável na tela seguida de um espaço.
+
+               - Incrementar nessa variável a sua data de nascimento e voltar a imprimir, seguida de um espaço.
+
+               Sugestão: Nomes auto-explicativos e clareza na escrita de código.
+
+
+         Exercício 2:
+
+              Uma viatura foi vista á 200 km/h em uma auto-estrada. _____ era a marca da viatura.
+
+              - Cria variaveis que guardem o valor da velocidade e a marca da viatura.
+
+              - Cria uma variável que guarde a variável da marca e depois altere a variável criada para outra marca.
+
+              - Cria uma variável que guarde a variável da velocidade. Converte-a para 200.0 km/h
+
+              - Imprima o seguinte texto preenchido com as variáveis declaradas:
+
+              "<marca> visto á <velocidade> km/h"
+              "Condutor: <seuNome>"
+
+              Obs.: Seu nome é uma variável.
+
+
+        Frase motivacional (opcional):
+
+        "A prática é mais importante do que pensares que sabes e não praticares" - Laércio Santos, 2024
+ */
+
+
+fun main() {
+
+}
