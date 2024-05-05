c18d4877230bf6d780887e54f1e5ef702b2c7569
Laércio Santos <laersantos13@hotmail.com>
2024-05-05 15:05:07 +0100

    Prólogo

---
+++ logica-programacao-kotlin 1/src/Prologo.kt
@@ -0,0 +1,38 @@
+/*
+    Boa! Conseguiste concluir o primeiro passo.
+
+    O princípio de qualquer coisa é sempre dificil mas lembra-te que são etapas que precisam de ser concluidas.
+
+    Para essa fase tens de:
+     - Ler e interpretar (Importantíssimo);
+     - Sentires-te confortável em imprimir informação na tela.
+
+    O que ganhas:
+     - Melhoramento de lógica programação em Kotlin;
+     - Noção do que fazer para tomada de decisões;
+     - Saber fazer o uso correto do <print()>, <println()> e do </n> dentro das duas instruções.
+
+    Introdução:
+
+    A lógica de programação representa 80% da tarefa em qualquer projeto. Aprimorar-la é tão importante
+    porque é dela que surgem as nossas dúvidas, sentido de pesquisa critico para resolução, e é onde
+    surge a nossa auto-confiança.
+
+    Exercício:
+
+        Imprimir dentro da fun main(){} :
+        - O porquê dela ser tão importante;
+        - A primeira frase do texto (Introdução);
+        - A tua opinição (Máx 2 linhas).
+
+        Sugestão:
+            - Quando existir uma vírgula no texto (,), imprimir o resto na linha seguinte;
+            - Dar dois espaços depois de imprimir cada tarefa;
+            - Ser o mais claro possível na utilização das intruções de impressão.
+
+ */
+
+
+fun main() {
+
+}
