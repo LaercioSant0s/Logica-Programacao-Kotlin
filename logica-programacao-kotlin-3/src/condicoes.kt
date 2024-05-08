8574b9919a9f8783d03b65454c526fc50b080a1a
Laércio Santos <laersantos13@hotmail.com>
2024-05-08 20:10:26 +0100

    Condicoes
    Please enter the commit message for your changes. Lines starting

---
+++ logica-programacao-kotlin-3/src/condicoes.kt
@@ -0,0 +1,71 @@
+/*
+
+    "Tudo o que aprendeste é e será sempre o fundamental para a resolução de qualquer projeto" Laércio Santos, 2024
+
+    Condições:
+
+    Condições são escolhas que o nosso programa toma para realização de uma tarefa. Podes pensar como se
+    fosse uma rua estreita e no final dessa rua surgem duas ou mais ruas em que apenas uma pode ser escolhida.
+
+    O condicionador para muitas línguagens de programação é o if(<condicao>){}
+
+    Para o caso do kotlin temos duas opções:
+
+    - "if()", seguido de um "else" caso escolha o outro caminho;
+
+    Extra: o if pode ser seguido sequencialmente de um "else if ()" para indicar mais de dois caminhos.
+    Sintaxe:
+
+    if (<condicao>) {
+
+        // o que acontecerá nesse caminho
+
+    } else if (<condicao>) { // Quantas vezes achar necessário
+
+        // o que acontecerá nesse caminho
+
+    } else {
+
+        // o que acontecerá nesse caminho, caso nenhum caminho acima tenha sido escolhido
+
+    }
+
+   - "When (<Condicao>)" desenvolvido para escolhas múltiplas com menos linhas de código:
+
+   sintaxe:
+
+   When (<condicao>) {
+
+       (se for isso) -> (faz isso)
+       (se for isso) -> (faz isso)
+       ...
+
+       else -> (faz isso)
+
+   }
+
+
+    Para essa fase tens de:
+            - Ler e interpretar (Importantíssimo);
+            - Criar um programa que satisfaça o pedido.
+
+            O que ganhas:
+             - Melhoramento de lógica programação;
+             - Decisão do fluxo do programa;
+             - Aperfeiçoar o que aprendeste;
+
+
+    Exercício:
+
+    Crie uma variável que guarde a média do somatório (20+17+18) (obs: conservar casas decimáis)
+
+    - Se essa média for superior a 18, altere o mesmo para 1.0 e imprima ("Alteração feita com sucesso para 1.0")
+    - Caso contrário, se a média for inferior a 16, altere o mesmo para 0.5 e imprima ("Alteração feita com sucesso para 0.5")
+    - Caso contrário imprima ("A média está entre 16 e 18")
+
+ */
+
+fun main() {
+
+
+}
