aa3b64f894de8154cd01287da8606b356d3c4e19
Laércio Santos <laersantos13@hotmail.com>
2024-05-11 11:47:13 +0100

    Ciclos

---
+++ src/Ciclos.kt
@@ -0,0 +1,26 @@
+/*
+
+    Ciclos:
+
+        Estrututuras que permitem repetir uma instrução enquanto a condição fôr verdadeira
+
+    Exercício 1:
+
+        - Escrever os números de 1 á 10 com um ciclo;
+        - escrever de forma inversa com um ciclo;
+
+    Exercício 2:
+
+        - O António tem 17 anos.
+
+        Enquanto o António tiver 17 anos, o ciclo repete e incrementa 1 ano.
+        Quando o António tiver 20 anos o ciclo para. Incrementa +1 na idade e imprime a idade do António.
+
+        Obs.: Ao imprimir deixar claro á quem pertence a idade.
+
+ */
+
+fun main() {
+
+
+}
