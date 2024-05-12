aae74b6d2b3f0b1f1a97a7a9ce936bbb47ad3810
Laércio Santos <laersantos13@hotmail.com>
2024-05-12 10:50:55 +0100

    funcoes

---
+++ logica-programacao-kotlin-5/src/funcoes.kt
@@ -0,0 +1,34 @@
+/*
+
+    Chegaste ao último tema, parabéns!
+
+    Funções: são estruturas auxiliares (funções), que realizam uma tarefa e retornam um valor.
+
+    Exercício 1:
+
+        - Crie uma função que retorne a soma de dois valores; (nome da função auto-explicativo)
+        - Na "fun main", invoque essa função, escolha dois valores para passar por parâmetro e imprima esse resultado.
+
+        - Crie uma função que retorne a frase "Nós estamos muito melhores"
+        - Na main guarde esse resultado em uma váriavel, de seguida imprima a variável.
+
+    Exercício 2:
+
+        - Crie uma função chamada "menu" que retorne o seguinte menu:
+
+
+        Bem vindo ao Campo Grande!
+
+        1 - Jardim
+        2 - Universidade
+        3 - McDonalds
+        4 - Ficar em casa
+        0 - sair
+
+        - Na main guarde o valor e o imprima. (nome da variável auto-explicativo)
+
+ */
+
+fun main() {
+
+}
